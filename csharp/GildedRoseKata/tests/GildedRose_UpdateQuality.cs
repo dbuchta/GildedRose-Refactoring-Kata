0a5b67a574dbb2cebd49e4d3503b810b9d4102c8
Drew Buchta <dbuchta14@gmail.com>
2023-08-09 14:37:49 -0500

    better test name

--- csharp/GildedRoseKata/tests/GildedRose_UpdateQuality.cs
+++ csharp/GildedRoseKata/tests/GildedRose_UpdateQuality.cs
@@ -32,7 +32,7 @@ namespace GildedRoseKata
         }
 
         [Fact]
-        public void Foo()
+        public void AddsNameToNewItem()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new(Items);
