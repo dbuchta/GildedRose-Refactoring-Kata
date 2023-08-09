4844acca45166daae6b2103dc949a50328c928f2
Drew Buchta <dbuchta14@gmail.com>
2023-08-09 14:36:59 -0500

    fixing original test

--- csharp/GildedRoseKata/tests/GildedRose_UpdateQuality.cs
+++ csharp/GildedRoseKata/tests/GildedRose_UpdateQuality.cs
@@ -37,7 +37,7 @@ namespace GildedRoseKata
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal("foo", Items[0].Name);
         }
     }
 }
